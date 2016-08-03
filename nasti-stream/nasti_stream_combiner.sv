d24b43177ed10a926f99b9c38f26dbdc512197ec
Gary Guo <nbdd0121@hotmail.com>
2016-08-03 14:09:36 +0100

    Extend NASTI-Stream combiner and slicer to allow max of 8 devices

--- nasti-stream/nasti_stream_combiner.sv
+++ nasti-stream/nasti_stream_combiner.sv
@@ -1,7 +1,7 @@
 module nasti_stream_combiner # (
    parameter N_PORT = 1
 ) (
-   nasti_stream_channel.slave  master_0, master_1, master_2, master_3,
+   nasti_stream_channel.slave  master_0, master_1, master_2, master_3, master_4, master_5, master_6, master_7,
    nasti_stream_channel.master slave
 );
 
@@ -52,6 +52,54 @@ generate
       assign slave.t_dest [3] = master_3.t_dest;
       assign slave.t_user [3] = master_3.t_user;
       assign master_3.t_ready = slave.t_ready[3];
+   end
+
+   if(N_PORT > 4) begin
+      assign slave.t_valid[4] = master_4.t_valid;
+      assign slave.t_data [4] = master_4.t_data;
+      assign slave.t_strb [4] = master_4.t_strb;
+      assign slave.t_keep [4] = master_4.t_keep;
+      assign slave.t_last [4] = master_4.t_last;
+      assign slave.t_id   [4] = master_4.t_id;
+      assign slave.t_dest [4] = master_4.t_dest;
+      assign slave.t_user [4] = master_4.t_user;
+      assign master_4.t_ready = slave.t_ready[4];
+   end
+
+   if(N_PORT > 5) begin
+      assign slave.t_valid[5] = master_5.t_valid;
+      assign slave.t_data [5] = master_5.t_data;
+      assign slave.t_strb [5] = master_5.t_strb;
+      assign slave.t_keep [5] = master_5.t_keep;
+      assign slave.t_last [5] = master_5.t_last;
+      assign slave.t_id   [5] = master_5.t_id;
+      assign slave.t_dest [5] = master_5.t_dest;
+      assign slave.t_user [5] = master_5.t_user;
+      assign master_5.t_ready = slave.t_ready[5];
+   end
+
+   if(N_PORT > 6) begin
+      assign slave.t_valid[6] = master_6.t_valid;
+      assign slave.t_data [6] = master_6.t_data;
+      assign slave.t_strb [6] = master_6.t_strb;
+      assign slave.t_keep [6] = master_6.t_keep;
+      assign slave.t_last [6] = master_6.t_last;
+      assign slave.t_id   [6] = master_6.t_id;
+      assign slave.t_dest [6] = master_6.t_dest;
+      assign slave.t_user [6] = master_6.t_user;
+      assign master_6.t_ready = slave.t_ready[6];
+   end
+
+   if(N_PORT > 7) begin
+      assign slave.t_valid[7] = master_7.t_valid;
+      assign slave.t_data [7] = master_7.t_data;
+      assign slave.t_strb [7] = master_7.t_strb;
+      assign slave.t_keep [7] = master_7.t_keep;
+      assign slave.t_last [7] = master_7.t_last;
+      assign slave.t_id   [7] = master_7.t_id;
+      assign slave.t_dest [7] = master_7.t_dest;
+      assign slave.t_user [7] = master_7.t_user;
+      assign master_7.t_ready = slave.t_ready[7];
    end
 endgenerate
 
--- nasti-stream/nasti_stream_slicer.sv
+++ nasti-stream/nasti_stream_slicer.sv
@@ -2,7 +2,7 @@ module nasti_stream_slicer # (
    parameter N_PORT = 1
 ) (
    nasti_stream_channel.slave  master,
-   nasti_stream_channel.master slave_0, slave_1, slave_2, slave_3
+   nasti_stream_channel.master slave_0, slave_1, slave_2, slave_3, slave_4, slave_5, slave_6, slave_7
 );
 
 generate
@@ -52,7 +52,55 @@ generate
       assign slave_3.t_dest    = master.t_dest [3];
       assign slave_3.t_user    = master.t_user [3];
       assign master.t_ready[3] = slave_3.t_ready;
-    end
+   end
+
+   if(N_PORT > 4) begin
+      assign slave_4.t_valid   = master.t_valid[4];
+      assign slave_4.t_data    = master.t_data [4];
+      assign slave_4.t_strb    = master.t_strb [4];
+      assign slave_4.t_keep    = master.t_keep [4];
+      assign slave_4.t_last    = master.t_last [4];
+      assign slave_4.t_id      = master.t_id   [4];
+      assign slave_4.t_dest    = master.t_dest [4];
+      assign slave_4.t_user    = master.t_user [4];
+      assign master.t_ready[4] = slave_4.t_ready;
+   end
+
+   if(N_PORT > 5) begin
+      assign slave_5.t_valid   = master.t_valid[5];
+      assign slave_5.t_data    = master.t_data [5];
+      assign slave_5.t_strb    = master.t_strb [5];
+      assign slave_5.t_keep    = master.t_keep [5];
+      assign slave_5.t_last    = master.t_last [5];
+      assign slave_5.t_id      = master.t_id   [5];
+      assign slave_5.t_dest    = master.t_dest [5];
+      assign slave_5.t_user    = master.t_user [5];
+      assign master.t_ready[5] = slave_5.t_ready;
+   end
+
+   if(N_PORT > 6) begin
+      assign slave_6.t_valid   = master.t_valid[6];
+      assign slave_6.t_data    = master.t_data [6];
+      assign slave_6.t_strb    = master.t_strb [6];
+      assign slave_6.t_keep    = master.t_keep [6];
+      assign slave_6.t_last    = master.t_last [6];
+      assign slave_6.t_id      = master.t_id   [6];
+      assign slave_6.t_dest    = master.t_dest [6];
+      assign slave_6.t_user    = master.t_user [6];
+      assign master.t_ready[6] = slave_6.t_ready;
+   end
+
+   if(N_PORT > 7) begin
+      assign slave_7.t_valid   = master.t_valid[7];
+      assign slave_7.t_data    = master.t_data [7];
+      assign slave_7.t_strb    = master.t_strb [7];
+      assign slave_7.t_keep    = master.t_keep [7];
+      assign slave_7.t_last    = master.t_last [7];
+      assign slave_7.t_id      = master.t_id   [7];
+      assign slave_7.t_dest    = master.t_dest [7];
+      assign slave_7.t_user    = master.t_user [7];
+      assign master.t_ready[7] = slave_7.t_ready;
+   end
 endgenerate
 
 endmodule
