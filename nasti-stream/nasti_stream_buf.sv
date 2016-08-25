6495ba6c16191104719243b3993b23e52744dbe7
Gary Guo <nbdd0121@hotmail.com>
2016-08-25 17:25:01 +0100

    Let fifo and nasti_stream_buf utilize BRAM

--- nasti-stream/nasti_stream_buf.sv
+++ nasti-stream/nasti_stream_buf.sv
@@ -11,96 +11,52 @@
    nasti_stream_channel.master dest
 );
 
+typedef struct packed unsigned {
+   logic [DATA_WIDTH-1:0]   data;
+   logic [DATA_WIDTH/8-1:0] strb;
+   logic [DATA_WIDTH/8-1:0] keep;
+   logic                    last;
+   logic [ID_WIDTH-1:0]     id  ;
+   logic [DEST_WIDTH-1:0]   dest;
+   logic [USER_WIDTH-1:0]   user;
+} Unit;
+
 localparam BUF_WIDTH = BUF_SIZE > 1 ? $clog2(BUF_SIZE) : 1;
 
-// Ring buffer constructs
-logic [DATA_WIDTH-1:0]   data_buf [0:BUF_SIZE-1];
-logic [DATA_WIDTH/8-1:0] strb_buf [0:BUF_SIZE-1];
-logic [DATA_WIDTH/8-1:0] keep_buf [0:BUF_SIZE-1];
-logic                    last_buf [0:BUF_SIZE-1];
-logic [ID_WIDTH-1:0]     id_buf   [0:BUF_SIZE-1];
-logic [DEST_WIDTH-1:0]   dest_buf [0:BUF_SIZE-1];
-logic [USER_WIDTH-1:0]   user_buf [0:BUF_SIZE-1];
-
-logic [BUF_WIDTH-1:0] readptr, writeptr;
-logic [BUF_WIDTH-1:0] readptr_new, writeptr_new;
-
-// Internal status
 logic full, empty;
-logic r_fire, w_fire;
+Unit read_unit, write_unit;
 
-// Function to move ring buffer pointer to next location
-function [BUF_WIDTH-1:0] incr(input [BUF_WIDTH-1:0] ptr);
-   incr = ptr == BUF_SIZE - 1 ? 0 : ptr + 1;
-endfunction
+fifo # (
+   .WIDTH (($bits(Unit)+7)/8*8),
+   .DEPTH (BUF_WIDTH)
+) fifo (
+   .aclk    (aclk          ),
+   .aresetn (aresetn       ),
+   .w_en    (src.t_valid   ),
+   .w_data  (write_unit    ),
+   .r_en    (dest.t_ready  ),
+   .r_data  (read_unit     ),
+   .full    (full          ),
+   .empty   (empty         )
+);
 
-assign r_fire = dest.t_ready && dest.t_valid;
-assign w_fire = src.t_ready && src.t_valid;
+assign write_unit.data = src.t_data;
+assign write_unit.strb = src.t_strb;
+assign write_unit.keep = src.t_keep;
+assign write_unit.last = src.t_last;
+assign write_unit.id   = src.t_id  ;
+assign write_unit.dest = src.t_dest;
+assign write_unit.user = src.t_user;
 
-assign readptr_new = r_fire ? incr(readptr) : readptr;
-assign writeptr_new = w_fire ? incr(writeptr) : writeptr;
+assign dest.t_data = read_unit.data;
+assign dest.t_strb = read_unit.strb;
+assign dest.t_keep = read_unit.keep;
+assign dest.t_last = read_unit.last;
+assign dest.t_id   = read_unit.id  ;
+assign dest.t_dest = read_unit.dest;
+assign dest.t_user = read_unit.user;
 
 assign dest.t_valid = !empty;
-assign src.t_ready = !full;
-
-always_ff @(posedge aclk or negedge aresetn)
-   if (!aresetn) begin
-      readptr <= 0;
-      writeptr <= 0;
-      full  <= 0;
-      empty <= 1;
-   end
-   else begin
-      readptr  <= readptr_new;
-      writeptr <= writeptr_new;
-
-      // Update empty & full from pointer
-      if (readptr_new == writeptr_new) begin
-         if (r_fire) begin
-            empty <= 1;
-            full  <= 0;
-         end else if (w_fire) begin
-            empty <= 0;
-            full  <= 1;
-         end
-      end
-      else begin
-         empty <= 0;
-         full <= 0;
-      end
-
-      if (w_fire && writeptr == readptr_new) begin
-         // Writing without buffer remaining
-         // Pipe it to output directly
-         dest.t_data <= src.t_data;
-         dest.t_strb <= src.t_strb;
-         dest.t_keep <= src.t_keep;
-         dest.t_last <= src.t_last;
-         dest.t_id   <= src.t_id  ;
-         dest.t_dest <= src.t_dest;
-         dest.t_user <= src.t_user;
-      end
-      else begin
-         // Fetch read data from buffer
-         dest.t_data <= data_buf[readptr_new];
-         dest.t_strb <= strb_buf[readptr_new];
-         dest.t_keep <= keep_buf[readptr_new];
-         dest.t_last <= last_buf[readptr_new];
-         dest.t_id   <= id_buf  [readptr_new];
-         dest.t_dest <= dest_buf[readptr_new];
-         dest.t_user <= user_buf[readptr_new];
-      end
-
-      // Write into buffer
-      if (w_fire) begin
-         data_buf[writeptr] <= src.t_data;
-         strb_buf[writeptr] <= src.t_strb;
-         keep_buf[writeptr] <= src.t_keep;
-         last_buf[writeptr] <= src.t_last;
-         id_buf  [writeptr] <= src.t_id  ;
-         dest_buf[writeptr] <= src.t_dest;
-         user_buf[writeptr] <= src.t_user;
-      end
-   end
+assign src.t_ready  = !full;
 
 endmodule
--- util/fifo.sv
+++ util/fifo.sv
@@ -13,12 +13,33 @@ module fifo # (
 );
 
 // Ring buffer constructs
-logic [WIDTH-1:0] buffer [0:2**DEPTH-1];
 logic [DEPTH-1:0] readptr, readptr_next, writeptr;
 logic [DEPTH-1:0] readptr_new, writeptr_new;
 
 // Internal status
 logic r_fire, w_fire;
+
+logic r_use_latch;
+logic [WIDTH-1:0] r_data_latch, r_data_read;
+
+dual_port_bram # (
+   .DATA_WIDTH (WIDTH),
+   .ADDR_WIDTH (DEPTH)
+) buffer (
+   .clk_a   (aclk),
+   .en_a    (r_fire && !(w_fire && writeptr == readptr_new)),
+   .we_a    ('0),
+   .addr_a  (readptr_next),
+   .write_a ('0),
+   .read_a  (r_data_read),
+
+   .clk_b   (aclk),
+   .en_b    (w_fire),
+   .we_b    ('1),
+   .addr_b  (writeptr),
+   .write_b (w_data),
+   .read_b  ()
+);
 
 // Function to move ring buffer pointer to next location
 function [DEPTH-1:0] incr(input [DEPTH-1:0] ptr);
@@ -30,6 +51,8 @@ assign w_fire = w_en & !full;
 
 assign readptr_new = r_fire ? readptr_next : readptr;
 assign writeptr_new = w_fire ? incr(writeptr) : writeptr;
+
+assign r_data = r_use_latch ? r_data_latch : r_data_read;
 
 always_ff @(posedge aclk or negedge aresetn)
    if (!aresetn) begin
@@ -54,17 +77,16 @@ always_ff @(posedge aclk or negedge aresetn)
          full <= 0;
       end
 
-      if (w_fire && writeptr == readptr_new)
+      if (w_fire && writeptr == readptr_new) begin
          // Writing without buffer remaining
          // Pipe it to output directly
-         r_data <= w_data;
-      else if (r_fire)
+         r_data_latch <= w_data;
+         r_use_latch <= 1;
+      end
+      else if (r_fire) begin
          // Fetch read data from buffer
-         r_data <= buffer[readptr_next];
-
-      // Write into buffer
-      if (w_fire)
-         buffer[writeptr] <= w_data;
+         r_use_latch <= 0;
+      end
    end
 
 endmodule
